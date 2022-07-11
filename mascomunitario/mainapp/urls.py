53eac59c2bca10cbc93927a9b8c2c7999ef663bf
daferarte <daferarte@gmail.com>
2022-07-10 20:57:51 -0500

    Agregando login de usuario

--- mascomunitario/mainapp/urls.py
+++ mascomunitario/mainapp/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('inicio/', views.index, name="inicio"),
+    path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_user, name="logout"),
 ]
