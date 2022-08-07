77dac1ff9816b1fe9a58ede8c88ac4dea46e7f4c
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-08-07 17:02:06 -0500

    ajuste modelo

--- mascomunitario/asistencia/models.py
+++ mascomunitario/asistencia/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from grupos.models import Grupos
-from django.contrib.auth.models import User
+from mainapp.models import Personas
 # Create your models here.
 class Listas(models.Model):
     cedula=models.CharField(max_length=50, verbose_name='Cedula')
@@ -20,8 +20,7 @@ class Listas(models.Model):
 class Horarios(models.Model):
     nombre=models.CharField(max_length=100, verbose_name='Numero clase')
     Fecha=models.DateTimeField(verbose_name='Fecha')
-    #user = models.ManyToManyField(User, verbose_name="Docentes", blank=True)
-    user=models.OneToOneField(User, editable=False, verbose_name="Usuario", on_delete=models.CASCADE) #clave relacional del modelo de usuarios de django
+    personas = models.ManyToManyField(Personas, verbose_name="Docentes", blank=True)
     create_at=models.DateTimeField(auto_now_add=True,verbose_name='Creado')
     update_at=models.DateTimeField(auto_now=True,verbose_name='Editado')
 
