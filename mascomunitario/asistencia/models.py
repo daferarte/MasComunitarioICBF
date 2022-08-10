8ae105ca103c18e02f488622b71e5177ec039c4a
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-08-10 09:12:34 -0500

    cambio de persona por usuario

--- mascomunitario/asistencia/models.py
+++ mascomunitario/asistencia/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from grupos.models import Grupos
-from mainapp.models import Personas
+from django.contrib.auth.models import User
+
 # Create your models here.
 class Listas(models.Model):
     cedula=models.CharField(max_length=50, verbose_name='Cedula')
@@ -20,7 +21,7 @@ class Listas(models.Model):
 class Horarios(models.Model):
     nombre=models.CharField(max_length=100, verbose_name='Numero clase')
     Fecha=models.DateTimeField(verbose_name='Fecha')
-    personas = models.ForeignKey(Personas, on_delete=models.CASCADE, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     create_at=models.DateTimeField(auto_now_add=True,verbose_name='Creado')
     update_at=models.DateTimeField(auto_now=True,verbose_name='Editado')
 
