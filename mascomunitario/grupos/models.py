1284fceda82c96f53b59323a65c7f5d68ffc8a0f
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-08-10 09:21:12 -0500

    Cambio de grupo de persona a usuario

--- mascomunitario/grupos/models.py
+++ mascomunitario/grupos/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-from mainapp.models import Personas
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Grupos(models.Model):
     nombre=models.CharField(max_length=100, verbose_name='Nombre')
     descripcion=models.CharField(max_length=150, verbose_name='Descripción')
-    personas = models.ManyToManyField(Personas, verbose_name="Docentes", blank=True)
+    usuario = models.ManyToManyField(User, verbose_name="Docentes", blank=True)
     create_at=models.DateTimeField(auto_now_add=True,verbose_name='Creado')
     update_at=models.DateTimeField(auto_now=True,verbose_name='Editado')
 
