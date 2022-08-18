8356483b573efcaefe12921a5e818555aef7a1cc
Daniel Arteaga <dfarteaga@unicesmag.edu.co>
2022-08-18 16:43:20 -0500

    Necesatio el login

--- mascomunitario/asistencia/views.py
+++ mascomunitario/asistencia/views.py
@@ -5,8 +5,10 @@ from grupos.models import Grupos
 from asistencia.models import Listas, Horarios, Asistencia
 from .forms import RegisterForm
 from django.contrib.auth.hashers import make_password
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
+@login_required(login_url='login/')
 def listasdeasistencia(request):
     docente=User.objects.filter(pk=request.user.id)[: 1]
     grupos=Grupos.objects.filter(usuario__id__in=docente)
@@ -49,6 +51,7 @@ def listasdeasistencia(request):
         'asistencia':asistentes
     })
 
+@login_required(login_url='login/')
 def guardarDatos(request):
     dat=['16727719','22793738','24339612','31320533','31710168','34502171','34513190','34772019','36758469','36954621','37083399','37087810','37275197','37686899','38560373','38602485','41956052','43602806','59312852','63547507','63557032','87061246','87067539','89008319','113665182','1004545915','1015411874','1049631763','1053824875','1061748566','1061792534','1081404295','1083891603','1083894795','1085246466','1085250401','1085262669','1085264688','1085280569','1085293619','1085305227','1085320329','1085325986','1085920621','1088596087','1096033717','1098604701','1122783881','1130639294','1130642292','1143935912']
     for i in dat:
@@ -58,7 +61,8 @@ def guardarDatos(request):
         )
         #print(user.password)
         user.save()
-    
+
+@login_required(login_url='login/')
 def registroHorario(request):
 
     #validacion para que redirija si esta registrado el usuario
